9c529054f755de517dd491a50753b9daaac7c69a
Steven Zeck <steven.zeck@gmail.com>
2018-05-06 18:31:56 -0500

    Delete files in dist

--- dist/test/pkg.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("../src");
-test("should successfully load package.json into pkg property of Beautifier", () => {
-    const name = "@unibeautify/beautifier-yapf";
-    expect(src_1.default.package).toHaveProperty("name", name);
-});
-//# sourceMappingURL=pkg.spec.js.map
--- dist/test/utils.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Wraps a string in a marker object that is used by `./raw-serializer.js` to
- * directly print that string in a snapshot without escaping all double quotes.
- * Backticks will still be escaped.
- */
-export declare function raw(text: string): {
-    [x: string]: string;
-};
